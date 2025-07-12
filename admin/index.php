e2b77176bade9ff7880e83a75ffdce74a30d7390
Hamzah82 <wokabi108@gmail.com>
2025-07-12 21:15:03 +0700

    update hash

--- admin/index.php
+++ admin/index.php
@@ -15,7 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = $_POST['password'] ?? '';
     // Hash password yang aman. Ganti 'SangatRahasia@2025!' dengan password Anda sendiri.
     // Untuk membuat hash baru, gunakan: echo password_hash('password_baru_anda', PASSWORD_DEFAULT);
-    $hashed_password = '$2y$12$3TrQe6oti4QDo7zpnIXE1eBnDPaxvdrV5BafQXYI7VU25XXUHx7Ti'; // Hash untuk 'Hamzah1089'
+    $hashed_password = '$2y$12$OU32l1kyz3qAP2O4OhwggOyY30LtNQICdMqEMypgWD8nmenKaKtn6'; // Hash untuk 'ADMIN#1234'
 
     if (password_verify($password, $hashed_password)) {
         session_regenerate_id(true); // Mencegah session fixation
